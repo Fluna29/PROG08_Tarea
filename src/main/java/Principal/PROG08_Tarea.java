ec2dd8856a863d496e5602f9d8fe388ecf8be690
Fluna29 <fluna293107@gmail.com>
2024-04-06 19:06:56 +0200

    Base Archivos

---
+++ src/main/java/Principal/PROG08_Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Principal;
+
+/**
+ *
+ * @author fluna
+ */
+public class PROG08_Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
