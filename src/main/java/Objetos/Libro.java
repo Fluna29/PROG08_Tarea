985241e28a8a161046dbd8a1dbaa5c1b262e8614
Fluna29 <fluna293107@gmail.com>
2024-04-06 19:51:33 +0200

    Creación clase objeto persona y clase objeto libro

--- src/main/java/Objetos/Libro.java
+++ src/main/java/Objetos/Libro.java
@@ -1,13 +1,116 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Objetos;
 
 /**
- *
- * @author fluna
+ * Esta clase permitirá almacenar la información de todos los libros del catálogo
+ * de la biblioteca.
+ * @author Francisco Luna Raya
  */
 public class Libro {
-    
+    private String isbn;
+    private String titulo;
+    private int anyoPublicacion;
+    private Persona autor;
+    private String genero;
+    private String sinopsis;
+
+    public Libro (String isbn, String titulo, int anyoPublicacion, Persona autor, String genero, String sinopsis){
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.anyoPublicacion = anyoPublicacion;
+        this.autor = autor;
+        this.genero = genero;
+        this.sinopsis = sinopsis;
+    }
+
+    /**
+     * @return String return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return String return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return int return the anyoPublicacion
+     */
+    public int getAnyoPublicacion() {
+        return anyoPublicacion;
+    }
+
+    /**
+     * @param anyoPublicacion the anyoPublicacion to set
+     */
+    public void setAnyoPublicacion(int anyoPublicacion) {
+        this.anyoPublicacion = anyoPublicacion;
+    }
+
+    /**
+     * @return Persona return the autor
+     */
+    public Persona getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(Persona autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return String return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return String return the sinopsis
+     */
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    /**
+     * @param sinopsis the sinopsis to set
+     */
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    /**
+     * @return Devuelve un String con los datos del libro
+     */
+    @Override
+    public String toString(){
+        return("Datos Libro {ISBN: " + getIsbn() + " | Título: " + getTitulo() + " | Año Publicación: " + getAnyoPublicacion() + " | Autor: " + getAutor() + " | Género: " + getGenero() + " | Sinopsis: " + getSinopsis() + " }");
+    }
 }
--- src/main/java/Objetos/Persona.java
+++ src/main/java/Objetos/Persona.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Objetos;
 
 /**
@@ -9,5 +5,47 @@ package Objetos;
  * @author fluna
  */
 public class Persona {
-    
+    private String nombre;
+    private String apellidos;
+
+    public Persona (String nombre, String apellidos){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return String return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return String return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return Devuelve un string con los datos del autor
+     */
+    @Override
+    public String toString(){
+        return("Datos Autor {Nombre: " + getNombre() + " | Apellidos" + getApellidos() +" }");
+    }
 }
